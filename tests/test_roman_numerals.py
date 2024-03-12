e511f9cf9c336b18d9ae6a3d6879aab6d978a71c
Abdur Rahman Abid <abdurrahmanabid@mt-mbp-mljw1m9904.fritz.box>
2024-03-12 11:17:59 +0000

    test 5 equals V passes

--- tests/test_roman_numerals.py
+++ tests/test_roman_numerals.py
@@ -5,11 +5,11 @@ def convert_to_roman(number):
         for i in range(number):
             roman_numeral += "I"
         return roman_numeral
-
     elif number == 12:
         return "XII"
-    else: 
+    elif number == 4: 
         return "IV"
+    else: return "V"
 
 # 1 = I
 def test_one_equals_I():
@@ -17,15 +17,18 @@ def test_one_equals_I():
 
 def test_two_equals_II():
     assert convert_to_roman(2) == "II"
+    
+def test_3_equals_III():
+    assert convert_to_roman(3) == "III"
 
 def test_4_equals_IV():
     assert convert_to_roman(4) == "IV"
 
+def test_5_equals_V():
+    assert convert_to_roman(5) == "V"
+
 def test_12_equals_XII():
     assert convert_to_roman(12) == "XII"
 
-def test_3_equals_III():
-    assert convert_to_roman(3) == "III"
 
-def test_5_equals_V():
-    assert convert_to_roman(5) == "V"
+
